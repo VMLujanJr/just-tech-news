286d472a87fd23e4ce2df7103792b0992328f3b6
VMLujanJr <vmlujanjr@outlook.com>
2022-05-17 16:02:02 -0500

    initial CRUD methods included

---
+++ routes/api/user-routes.js
@@ -0,0 +1,110 @@
+const router = require('express').Router();
+const req = require('express/lib/request');
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method
+    User.findAll() // findAll() is the Model's class's method... similar to SELECT * FROM users;
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: { // SELECT * FROM users WHERE id = 1;
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post('/', (req, res) => {
+    // expects { username: 'Victor', email: 'vmlujanjr@outlook.com', password: 'pass123' } // we defined these in models/User.js
+
+    /* In MySQL the command looks like this... */
+    /* 
+    INSERT INTO users
+        (username, email, password)
+    VALUES
+        ('Victor', 'vmlujanjr@outlook.com', 'pass123');
+    */
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+    // expects { username: 'Victor', email: 'vmlujanjr@outlook.com', password: 'pass123' }
+
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    /* req.body to provide new data we want to use in the update & req.params.id to indicate where exactly we want that new data to be used */
+    /*
+    UPDATE users
+    SET username = 'Victor', email = 'vmlujanjr@outlook.com', password = 'pass123'
+    WHERE id = 1;
+    */
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (res, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
+
+/* REST = Representational State Transfer */
