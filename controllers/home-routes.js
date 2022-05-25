ebbd6cca59ce2e1a732ba460e7546ea59a779bcf
VMLujanJr <vmlujanjr@outlook.com>
2022-05-25 14:54:23 -0500

    implemented handbars engine, included initial homepage template

---
+++ controllers/home-routes.js
@@ -0,0 +1,57 @@
+const sequelize = require('../config/connection.js');
+const { Post, User, Comment } = require('../models');
+const router = require('express').Router();
+
+/* router.get('/', (req, res) => { // ...test
+    res.render('homepage', { // handlebars extension is implied
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        vote_count: 10,
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
+}); */
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal(`(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // pass a single post object into the homepage template
+        // serialize entire array
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        /* console.log(dbPostData[0]); */
+        /* res.render('homepage', dbPostData[0].get({ plain: true })); */
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
-
+router.use('/', homeRoutes);
 /* if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource, another RESTful API practice. */
 router.use((req, res) => {
     res.status(404).end();
